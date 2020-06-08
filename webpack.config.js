275c80809ada6f13ae126cc68946a3dd838e4a9a
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-06-08 08:30:11 -0500

    Added home files to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,128 @@
 const autoprefixer = require('autoprefixer');
 
-module.exports = {
-  entry: ['./assets/scss/login.scss', './login.js'],
-  output: {
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'bundle.css',
-            },
-          },
-          {loader: 'extract-loader'},
-          {loader: 'css-loader'},
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [autoprefixer()]
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              // Prefer Dart Sass
-              implementation: require('sass'),
-
-              // See https://github.com/webpack-contrib/sass-loader/issues/804
-              webpackImporter: false,
-              sassOptions: {
-                includePaths: ['./node_modules'],
-              },
-            },
-          }
-        ],
+function getStyleUse(bundleFilename) {
+  return [
+    {
+      loader: 'file-loader',
+      options: {
+        name: bundleFilename,
       },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env'],
-        },
+    },
+    { loader: 'extract-loader' },
+    { loader: 'css-loader' },
+    {
+      loader: 'postcss-loader',
+      options: {
+        plugins: () => [autoprefixer()]
       }
-    ],
+    },
+    {
+      loader: 'sass-loader',
+      options: {
+        //Prefer Dart Implementation
+        implementation: require('sass'),
+        webpackImporter: false,
+        sassOptions: {
+          includePaths: ['./node_modules'],
+        }
+      },
+    }
+  ];
+}
+
+// [
+//   {
+//     loader: 'file-loader',
+//     options: {
+//       name: 'bundle.css',
+//     },
+//   },
+//   {loader: 'extract-loader'},
+//   {loader: 'css-loader'},
+//   {
+//     loader: 'postcss-loader',
+//     options: {
+//       plugins: () => [autoprefixer()]
+//     }
+//   },
+//   {
+//     loader: 'sass-loader',
+//     options: {
+//       // Prefer Dart Sass
+//       implementation: require('sass'),
+
+//       // See https://github.com/webpack-contrib/sass-loader/issues/804
+//       webpackImporter: false,
+//       sassOptions: {
+//         includePaths: ['./node_modules'],
+//       },
+//     },
+//   }
+// ],
+// },
+// {
+// test: /\.js$/,
+// loader: 'babel-loader',
+// query: {
+//   presets: ['@babel/preset-env'],
+// },
+// }
+// ],
+// },
+// }
+
+module.exports = [
+  {
+    entry: './assets/scss/login.scss',
+    output: {
+      // This is necessary for webpack to compile, but we never reference this js file.
+      filename: 'style-bundle-login.js',
+    },
+    module: {
+      rules: [{
+        test: /\login.scss$/,
+        use: getStyleUse('bundle-login.css')
+      }]
+    },
   },
-};
+  {
+    entry: "./login.js",
+    output: {
+      filename: "bundle-login.js"
+    },
+    module: {
+      loaders: [{
+        test: /login.js$/,
+        loader: 'babel-loader',
+        query: {presets: ['env']}
+      }]
+    },
+  },
+  {
+    entry: './assets/scss/home.scss',
+    output: {
+      // This is necessary for webpack to compile, but we never reference this js file.
+      filename: 'style-bundle-home.js',
+    },
+    module: {
+      rules: [{
+        test: /home.scss$/,
+        use: getStyleUse('bundle-home.css')
+      }]
+    },
+  },
+  {
+    entry: "./home.js",
+    output: {
+      filename: "bundle-home.js"
+    },
+    module: {
+      loaders: [{
+        test: /home.js$/,
+        loader: 'babel-loader',
+        query: {presets: ['env']}
+      }]
+    },
+  }
+
+]
